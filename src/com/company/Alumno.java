7b23c39e0ff8c3c75cd1abd744538d61da5030ab
Juan Manuel <juanmanuelpedro95@gmail.com>
2020-03-24 18:29:33 -0300

    gitignore
    ignore y constructor de alumno

--- src/com/company/Alumno.java
+++ src/com/company/Alumno.java
@@ -5,4 +5,10 @@ public class Alumno {
     private String nombre;
     private String apellido;
     private Integer codigoDeAlumno;
+
+    public Alumno(String nombre, String apellido, Integer codigoDeAlumno) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.codigoDeAlumno = codigoDeAlumno;
+    }
 }
