9ed7d08c39920a57d37ca56f6514546dbb1f83b0
Juan Manuel <juanmanuelpedro95@gmail.com>
2020-03-28 04:30:23 -0300

    PARTE J
    parte j con errores

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DigitalHouseManager digitalHouseManager = new DigitalHouseManager();
+
+        digitalHouseManager.altaProfesorTitular("Albus","Dumbledore", 001, "Artes Oscuras");
+        digitalHouseManager.altaProfesorTitular("Severus", "Snape", 002, "pociones");
+        digitalHouseManager.altaProfesorAdjunto("Remus", "Lupin", 003);
+        digitalHouseManager.altaProfesorAdjunto("Dolores", "Umbridge", 004);
+
+        digitalHouseManager.altaCurso("Full Stack", 20001, 3);
+        digitalHouseManager.altaCurso("Android", 20002, 2);
+
+        digitalHouseManager.asignarProfesores(20001, 001, 003);
+        digitalHouseManager.asignarProfesores(20002, 002, 004);
+
+        digitalHouseManager.altaAlumno("Harry", "Potter", 100);
+        digitalHouseManager.altaAlumno("Ronald", "Weasley", 101);
+        digitalHouseManager.altaAlumno("Hermione", "Granger", 102);
+
+        digitalHouseManager.inscribirAlumno(100, 20001);
+        digitalHouseManager.inscribirAlumno(101, 20001);
+        digitalHouseManager.inscribirAlumno(100, 20002);
+        digitalHouseManager.inscribirAlumno(101, 20002);
+        digitalHouseManager.inscribirAlumno(102, 20002);
+
+        digitalHouseManager.bajaProfesor(004);
+
+        digitalHouseManager.bajaCurso(20001);
+
+
+
+
+
+
     }
 }
--- src/com/company/ProfesorAdjunto.java
+++ src/com/company/ProfesorAdjunto.java
@@ -1,9 +1,15 @@
 package com.company;
 
 public class ProfesorAdjunto extends Profesor {
-    public ProfesorAdjunto(String nombre, String apellido, Integer codigoDeProfesor) {
+    public Integer horasdeConsultas;
+
+    public ProfesorAdjunto(String nombre, String apellido, Integer codigoDeProfesor, Integer horasdeConsultas) {
         super(nombre, apellido, codigoDeProfesor);
+        this.horasdeConsultas = horasdeConsultas;
+    }
+    public Integer getHorasdeConsultas(){
+        return horasdeConsultas;
     }
 
-    public Integer horasdeConsultas;
+
 }
